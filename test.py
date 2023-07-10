29ef54c6a3196eee081ce1fb7191441331ce6cbf
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2023-07-10 13:27:11 -0700

    testing the api within the test file

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello')
+import requests
+    
+key = 'W626K7TKR5CCR5UT'
+url = 'https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo'
+response = requests.get(url)
+data = response.json()
+
+print(response)
+print(data)
